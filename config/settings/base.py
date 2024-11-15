25beaa951150e7c55c3fc4ea859231a97e642b71
ObjectLimit <greeenbane@proton.me>
2024-11-15 16:58:20 +0300

    feat: add TimeStamped,User,Profile models and signals

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import cloudinary
 import environ
 
 env = environ.Env(DEBUG=(bool, False))
@@ -114,6 +115,18 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
+
+CLOUDINARY_CLOUD_NAME = env("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = env("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = env("CLOUDINARY_API_SECRET")
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+)
 
 LOGGING = {
     "version": 1,
